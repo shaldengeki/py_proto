6a4b2b078328b1e964e5e61b2af3c9adb1e71eb9
Charles OuGuo <shaldengeki@gmail.com>
2022-11-22 00:18:26 -0500

    Tests should depend directly on proto type classes, not Parser (#13)
    - Split proto_file up into submodules
    - import_test relies directly on ProtoImport
    - Make package only depend on ProtoPackage
    - Add tests
    - syntax_test depends on ProtoSyntax

--- src/proto_package.py
+++ src/proto_package.py
@@ -18,15 +18,24 @@ class ProtoPackage(ProtoNode):
 
     @staticmethod
     def match(proto_source: str) -> Optional["ParsedProtoNode"]:
-        if not proto_source.startswith("package "):
+        if not proto_source.startswith("package"):
             return None
+
+        if not proto_source.startswith("package "):
+            raise ValueError(f"Proto has invalid package: {proto_source}")
+
         proto_source = proto_source[8:]
         parts = proto_source.split(";")
         package = parts[0]
+
         if len(parts) == 1:
             raise ValueError(
                 f"Proto has invalid package declaration syntax: {';'.join(parts)}"
             )
+
+        if not package:
+            raise ValueError(f"Proto cannot have empty package: {proto_source}")
+
         proto_source = ";".join(parts[1:])
 
         if package.startswith(".") or package.endswith("."):
--- test/syntax_test.py
+++ test/syntax_test.py
@@ -1,54 +1,51 @@
 import unittest
-from src.parser import Parser
-from src.proto_syntax import ProtoSyntaxTypes
+from src.proto_syntax import ProtoSyntax, ProtoSyntaxTypes
 
 
 class SyntaxTest(unittest.TestCase):
     def test_correct_syntax(self):
         self.assertEqual(
-            Parser.loads("syntax = 'proto3'").syntax, ProtoSyntaxTypes.PROTO3
+            ProtoSyntax.match("syntax = 'proto3'").node, ProtoSyntaxTypes.PROTO3
         )
         self.assertEqual(
-            Parser.loads('syntax = "proto3"').syntax, ProtoSyntaxTypes.PROTO3
+            ProtoSyntax.match('syntax = "proto3"').node, ProtoSyntaxTypes.PROTO3
         )
         self.assertEqual(
-            Parser.loads("syntax = 'proto2'").syntax, ProtoSyntaxTypes.PROTO2
+            ProtoSyntax.match("syntax = 'proto2'").node, ProtoSyntaxTypes.PROTO2
         )
         self.assertEqual(
-            Parser.loads('syntax = "proto2"').syntax, ProtoSyntaxTypes.PROTO2
+            ProtoSyntax.match('syntax = "proto2"').node, ProtoSyntaxTypes.PROTO2
         )
 
     def test_syntax_not_present(self):
-        with self.assertRaises(Exception):
-            Parser.loads("")
-        with self.assertRaises(Exception):
-            Parser.loads('import "foo.proto";')
+        self.assertIsNone(ProtoSyntax.match(""))
+        self.assertIsNone(ProtoSyntax.match('import "foo.proto";'))
 
     def test_syntax_malformed(self):
-        with self.assertRaises(Exception):
-            Parser.loads("syntax = proto3")
-        with self.assertRaises(Exception):
-            Parser.loads("syntax = proto3;")
-        with self.assertRaises(Exception):
-            Parser.loads("syntax = 'proto3")
-        with self.assertRaises(Exception):
-            Parser.loads("syntax = 'proto3;")
-        with self.assertRaises(Exception):
-            Parser.loads("syntax = proto3'")
-        with self.assertRaises(Exception):
-            Parser.loads("syntax = proto3';")
-        with self.assertRaises(Exception):
-            Parser.loads("syntax = 'proton';")
-        with self.assertRaises(Exception):
-            Parser.loads("syntax = 'proton'")
-        with self.assertRaises(Exception):
-            Parser.loads("syntax = 'proton")
-        with self.assertRaises(Exception):
-            Parser.loads("syntax = proton';")
-        with self.assertRaises(Exception):
-            Parser.loads("syntax = proton'")
-        with self.assertRaises(Exception):
-            Parser.loads("syntax = proton")
+        with self.assertRaises(ValueError):
+            ProtoSyntax.match("syntax = proto3")
+        with self.assertRaises(ValueError):
+            ProtoSyntax.match("syntax = proto3;")
+        with self.assertRaises(ValueError):
+            ProtoSyntax.match("syntax = 'proto3")
+        with self.assertRaises(ValueError):
+            ProtoSyntax.match("syntax = 'proto3;")
+        with self.assertRaises(ValueError):
+            ProtoSyntax.match("syntax = proto3'")
+        with self.assertRaises(ValueError):
+            ProtoSyntax.match("syntax = proto3';")
+        with self.assertRaises(ValueError):
+            ProtoSyntax.match("syntax = 'proton';")
+        with self.assertRaises(ValueError):
+            ProtoSyntax.match("syntax = 'proton'")
+        with self.assertRaises(ValueError):
+            ProtoSyntax.match("syntax = 'proton")
+        with self.assertRaises(ValueError):
+            ProtoSyntax.match("syntax = proton';")
+        with self.assertRaises(ValueError):
+            ProtoSyntax.match("syntax = proton'")
+        with self.assertRaises(ValueError):
+            ProtoSyntax.match("syntax = proton")
 
 
 if __name__ == "__main__":
