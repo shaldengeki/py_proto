0d8a40f8ccef054c40cde56d29f09f361a6a756b
Charles Guo <shaldengeki@gmail.com>
2022-11-22 15:33:39 -0500

    Add support for empty enums

---
+++ test/proto_enum_test.py
@@ -0,0 +1,28 @@
+import unittest
+from textwrap import dedent
+
+from src.proto_enum import ProtoEnum
+from src.proto_identifier import ProtoIdentifier
+
+
+class EnumTest(unittest.TestCase):
+    def test_empty_enum(self):
+        parsed_empty_enum = ProtoEnum.match("""enum FooEnum {}""")
+        self.assertIsNotNone(parsed_empty_enum)
+        self.assertEqual(parsed_empty_enum.node.name, ProtoIdentifier("FooEnum"))
+
+        parsed_spaced_enum = ProtoEnum.match(
+            dedent(
+                """
+            enum FooEnum {
+
+            }
+        """.strip()
+            )
+        )
+        self.assertIsNotNone(parsed_spaced_enum)
+        self.assertEqual(parsed_spaced_enum.node.name, ProtoIdentifier("FooEnum"))
+
+
+if __name__ == "__main__":
+    unittest.main()
