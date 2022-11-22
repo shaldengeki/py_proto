d0303bcf715cad34a012c41a157c7daf18abe644
Charles OuGuo <shaldengeki@gmail.com>
2022-11-22 16:07:50 -0500

    In enums, support options (#23)
    - Refactor enum-parsing, loop ProtoOption in
    - Add tests for options in enums

--- test/proto_enum_test.py
+++ test/proto_enum_test.py
@@ -1,8 +1,12 @@
 import unittest
 from textwrap import dedent
 
+from src.proto_bool import ProtoBool
+from src.proto_constant import ProtoConstant
 from src.proto_enum import ProtoEnum
 from src.proto_identifier import ProtoIdentifier
+from src.proto_option import ProtoOption
+from src.proto_string_literal import ProtoStringLiteral
 
 
 class EnumTest(unittest.TestCase):
@@ -36,6 +40,34 @@ class EnumTest(unittest.TestCase):
         )
         self.assertIsNotNone(empty_statement_enum)
         self.assertEqual(empty_statement_enum.node.name, ProtoIdentifier("FooEnum"))
+
+    def test_enum_optionals(self):
+        parsed_enum_with_optionals = ProtoEnum.match(
+            dedent(
+                """
+            enum FooEnum {
+                option java_package = "foobar";
+                option (foo.bar).baz = false;
+            }
+        """.strip()
+            )
+        )
+        self.assertIsNotNone(
+            parsed_enum_with_optionals.node.options,
+            [
+                ProtoOption(
+                    ProtoIdentifier("java_package"),
+                    ProtoConstant(ProtoStringLiteral("foobar")),
+                ),
+                ProtoOption(
+                    ProtoIdentifier("(foo.bar).baz"),
+                    ProtoConstant(ProtoBool(False)),
+                ),
+            ],
+        )
+        self.assertEqual(
+            parsed_enum_with_optionals.node.name, ProtoIdentifier("FooEnum")
+        )
 
 
 if __name__ == "__main__":
