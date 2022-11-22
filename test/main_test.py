0eb628bcd223a8c156ce8e74285c236f533b54bc
Charles OuGuo <shaldengeki@gmail.com>
2022-11-21 21:26:38 -0500

    Remove main targets (#8)
    See title.

--- test/main_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from src.main import main
-
-class MainTest(unittest.TestCase):
-    def test_main(self):
-        self.assertEqual(main(), 0)
-
-if __name__ == '__main__':
-    unittest.main()
