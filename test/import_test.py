e4a421fd383418e0deddc206eb7e3fdb76501325
Charles Guo <shaldengeki@gmail.com>
2022-11-21 23:59:19 -0500

    import_test relies directly on ProtoImport

--- test/import_test.py
+++ test/import_test.py
@@ -1,241 +1,104 @@
 import unittest
 from textwrap import dedent
-from src.parser import Parser, ParseError
 from src.proto_import import ProtoImport
 
 
 class ImportTest(unittest.TestCase):
-    def test_empty_imports(self):
-        self.assertEqual(Parser.loads("syntax = 'proto3'").imports, [])
-
     def test_bare_import(self):
-        self.assertEqual(
-            Parser.loads(
-                dedent(
-                    """syntax = 'proto3';
-            import "foo.proto";
-        """
-                )
-            ).imports,
-            [ProtoImport("foo.proto")],
-        )
-        self.assertEqual(
-            Parser.loads(
-                dedent(
-                    """syntax = 'proto3';
-            import 'foo.proto';
-        """
-                )
-            ).imports,
-            [ProtoImport("foo.proto")],
-        )
+        double_quote_import = ProtoImport.match('import "foo.proto";')
+        self.assertEqual(double_quote_import.node.path, "foo.proto")
+        self.assertEqual(double_quote_import.node.weak, False)
+        self.assertEqual(double_quote_import.node.public, False)
+        self.assertEqual(double_quote_import.remaining_source, "")
+
+        single_quote_import = ProtoImport.match("import 'foo.proto';")
+        self.assertEqual(single_quote_import.node.path, "foo.proto")
+        self.assertEqual(single_quote_import.node.weak, False)
+        self.assertEqual(single_quote_import.node.public, False)
+        self.assertEqual(single_quote_import.remaining_source, "")
 
     def test_import_missing_semicolon(self):
-        with self.assertRaises(ParseError):
-            Parser.loads(
-                dedent(
-                    """syntax = 'proto3';
-                import "foo.proto"
-            """
-                )
-            )
+        with self.assertRaises(ValueError):
+            ProtoImport.match('import "foo.proto"')
 
     def test_import_missing_starting_quote(self):
-        with self.assertRaises(ParseError):
-            Parser.loads(
-                dedent(
-                    """syntax = 'proto3';
-                import foo.proto";
-            """
-                )
-            )
-        with self.assertRaises(ParseError):
-            Parser.loads(
-                dedent(
-                    """syntax = 'proto3';
-                import foo.proto';
-            """
-                )
-            )
+        with self.assertRaises(ValueError):
+            ProtoImport.match('import foo.proto";')
+
+        with self.assertRaises(ValueError):
+            ProtoImport.match("import foo.proto';")
 
     def test_import_missing_ending_quote(self):
-        with self.assertRaises(ParseError):
-            Parser.loads(
-                dedent(
-                    """syntax = 'proto3';
-                import "foo.proto;
-            """
-                )
-            )
-        with self.assertRaises(ParseError):
-            Parser.loads(
-                dedent(
-                    """syntax = 'proto3';
-                import 'foo.proto;
-            """
-                )
-            )
+        with self.assertRaises(ValueError):
+            ProtoImport.match('import "foo.proto;')
 
-    def test_weak_default(self):
-        self.assertEqual(
-            Parser.loads(
-                dedent(
-                    """syntax = 'proto3';
-                import "foo.proto";
-                """
-                )
-            ).imports,
-            [ProtoImport("foo.proto", weak=False)],
-        )
+        with self.assertRaises(ValueError):
+            ProtoImport.match("import 'foo.proto;")
 
     def test_weak_import(self):
-        self.assertEqual(
-            Parser.loads(
-                dedent(
-                    """syntax = 'proto3';
-                import weak "foo.proto";
-                """
-                )
-            ).imports,
-            [ProtoImport("foo.proto", weak=True)],
-        )
-        self.assertEqual(
-            Parser.loads(
-                dedent(
-                    """syntax = 'proto3';
-                import weak 'foo.proto';
-                """
-                )
-            ).imports,
-            [ProtoImport("foo.proto", weak=True)],
-        )
+        self.assertEqual(ProtoImport.match('import weak "foo.proto";').node.weak, True)
+        self.assertEqual(ProtoImport.match("import weak 'foo.proto';").node.weak, True)
 
     def test_weak_typoed_import(self):
-        with self.assertRaises(ParseError):
-            Parser.loads(
-                dedent(
-                    """syntax = 'proto3';
-                    import weac "foo.proto";
-                """
-                )
-            )
+        with self.assertRaises(ValueError):
+            ProtoImport.match('import weac "foo.proto";')
 
     def test_weak_mixed_imports(self):
-        self.assertEqual(
-            Parser.loads(
-                dedent(
-                    """syntax = 'proto3';
-                import "foo.proto";
-                import weak "bar/baz.proto";
-                import "bat.proto";
-                """
-                )
-            ).imports,
-            [
-                ProtoImport("foo.proto", weak=False),
-                ProtoImport("bar/baz.proto", weak=True),
-                ProtoImport("bat.proto", weak=False),
-            ],
+        first_parsed_import = ProtoImport.match(
+            dedent(
+                """import "foo.proto";
+            import weak "bar/baz.proto";
+            import "bat.proto";"""
+            )
         )
+        self.assertEqual(first_parsed_import.node.path, "foo.proto")
+        self.assertEqual(first_parsed_import.node.weak, False)
 
-    def test_public_default(self):
-        self.assertEqual(
-            Parser.loads(
-                dedent(
-                    """syntax = 'proto3';
-                import "foo.proto";
-                """
-                )
-            ).imports,
-            [ProtoImport("foo.proto", public=False)],
-        )
+        second_parsed_import = ProtoImport.match(first_parsed_import.remaining_source)
+        self.assertEqual(second_parsed_import.node.path, "bar/baz.proto")
+        self.assertEqual(second_parsed_import.node.weak, True)
+
+        third_parsed_import = ProtoImport.match(second_parsed_import.remaining_source)
+        self.assertEqual(third_parsed_import.node.path, "bat.proto")
+        self.assertEqual(third_parsed_import.node.weak, False)
 
     def test_public_import(self):
         self.assertEqual(
-            Parser.loads(
-                dedent(
-                    """syntax = 'proto3';
-                import public "foo.proto";
-                """
-                )
-            ).imports,
-            [ProtoImport("foo.proto", public=True)],
+            ProtoImport.match('import public "foo.proto";').node.public, True
         )
         self.assertEqual(
-            Parser.loads(
-                dedent(
-                    """syntax = 'proto3';
-                import public 'foo.proto';
-                """
-                )
-            ).imports,
-            [ProtoImport("foo.proto", public=True)],
+            ProtoImport.match("import public 'foo.proto';").node.public, True
         )
 
     def test_public_typoed_import(self):
-        with self.assertRaises(ParseError):
-            Parser.loads(
-                dedent(
-                    """syntax = 'proto3';
-                    import publik "foo.proto";
-                """
-                )
-            )
+        with self.assertRaises(ValueError):
+            ProtoImport.match('import publik "foo.proto";')
 
     def test_public_weak_mutually_exclusive(self):
-        with self.assertRaises(ParseError):
-            Parser.loads(
-                dedent(
-                    """syntax = 'proto3';
-                    import weak public "foo.proto";
-                """
-                )
-            )
+        with self.assertRaises(ValueError):
+            ProtoImport.match('import weak public "foo.proto";')
 
-        with self.assertRaises(ParseError):
-            Parser.loads(
-                dedent(
-                    """syntax = 'proto3';
-                    import public weak "foo.proto";
-                """
-                )
-            )
+        with self.assertRaises(ValueError):
+            ProtoImport.match('import public weak "foo.proto";')
 
     def test_public_mixed_imports(self):
-        self.assertEqual(
-            Parser.loads(
-                dedent(
-                    """syntax = 'proto3';
-                import "foo.proto";
-                import public "bar/baz.proto";
-                import public "bat.proto";
-                """
-                )
-            ).imports,
-            [
-                ProtoImport("foo.proto", weak=False),
-                ProtoImport("bar/baz.proto", weak=False, public=True),
-                ProtoImport("bat.proto", weak=False, public=True),
-            ],
+        first_parsed_import = ProtoImport.match(
+            dedent(
+                """import "foo.proto";
+            import public "bar/baz.proto";
+            import public "bat.proto";"""
+            )
         )
+        self.assertEqual(first_parsed_import.node.path, "foo.proto")
+        self.assertEqual(first_parsed_import.node.public, False)
 
-    def test_multiple_imports(self):
-        self.assertEqual(
-            Parser.loads(
-                dedent(
-                    """syntax = 'proto3';
-            import "multiple_imports_1.proto";
-            import 'multiple_imports/2.proto';
-            import "multiple_imports_3.proto";
-        """
-                )
-            ).imports,
-            [
-                ProtoImport("multiple_imports_1.proto"),
-                ProtoImport("multiple_imports/2.proto"),
-                ProtoImport("multiple_imports_3.proto"),
-            ],
-        )
+        second_parsed_import = ProtoImport.match(first_parsed_import.remaining_source)
+        self.assertEqual(second_parsed_import.node.path, "bar/baz.proto")
+        self.assertEqual(second_parsed_import.node.public, True)
+
+        third_parsed_import = ProtoImport.match(second_parsed_import.remaining_source)
+        self.assertEqual(third_parsed_import.node.path, "bat.proto")
+        self.assertEqual(third_parsed_import.node.public, True)
 
 
 if __name__ == "__main__":
