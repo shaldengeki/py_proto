a03219348b38ede51a9d833c5d6e1f2b3d0c7d38
Charles OuGuo <shaldengeki@gmail.com>
2023-01-24 21:54:59 -0600

    In parser_binary_test, diff serialized protos

--- test/parser_binary_test.sh
+++ test/parser_binary_test.sh
@@ -5,12 +5,14 @@ echo "Local protos:"
 LOCAL_PROTOS=$(find ./test/resources -name "*.proto" | sort)
 for f in $LOCAL_PROTOS; do
     echo $f
-    ./src/parser_binary $f > /dev/null
+    ./src/parser_binary $f > "${f}_serialized.proto"
+    diff --side-by-side --ignore-all-space $f "${f}_serialized.proto"
 done
 
 echo "Google protos:"
 GOOGLE_PROTOS=$(find ./external/com_google_protobuf/src/google/protobuf -name "*.proto" | xargs grep --files-without-match "proto2" | sort)
 for f in $GOOGLE_PROTOS; do
     echo $f
-    ./src/parser_binary $f > /dev/null
+    ./src/parser_binary $f > "${f}_serialized.proto"
+    diff --side-by-side --ignore-all-space $f "${f}_serialized.proto"
 done
