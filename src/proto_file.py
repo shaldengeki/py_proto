144945d2106514f1e594c963df28e7d4e1e3edb0
Charles OuGuo <shaldengeki@gmail.com>
2023-03-08 11:02:19 -0400

    Make ProtoFile a ProtoNode subclass

--- src/proto_file.py
+++ src/proto_file.py
@@ -1,16 +1,29 @@
 from typing import Optional, Sequence
 
+from src.proto_comment import (
+    ProtoComment,
+    ProtoMultiLineComment,
+    ProtoSingleLineComment,
+)
 from src.proto_enum import ProtoEnum
+from src.proto_extend import ProtoExtend
 from src.proto_import import ProtoImport
 from src.proto_message import ProtoMessage
-from src.proto_node import ProtoNode, ProtoNodeDiff
+from src.proto_node import ParsedProtoNode, ProtoNode, ProtoNodeDiff
 from src.proto_option import ProtoOption
 from src.proto_package import ProtoPackage
+from src.proto_service import ProtoService
 from src.proto_syntax import ProtoSyntax
 
 
-class ProtoFile:
-    def __init__(self, syntax: ProtoSyntax, nodes: list[ProtoNode]):
+class ParsedProtoFileNode(ParsedProtoNode):
+    node: "ProtoFile"
+    remaining_source: str
+
+
+class ProtoFile(ProtoNode):
+    def __init__(self, syntax: ProtoSyntax, nodes: list[ProtoNode], *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.syntax = syntax
         self.nodes = nodes
 
@@ -39,6 +52,87 @@ class ProtoFile:
     @property
     def messages(self) -> list[ProtoMessage]:
         return [node for node in self.nodes if isinstance(node, ProtoMessage)]
+
+    @staticmethod
+    def parse_partial_content(partial_proto_content: str) -> ParsedProtoNode:
+        node_types: list[type[ProtoNode]] = [
+            ProtoImport,
+            ProtoMessage,
+            ProtoPackage,
+            ProtoOption,
+            ProtoEnum,
+            ProtoExtend,
+            ProtoService,
+            ProtoSingleLineComment,
+            ProtoMultiLineComment,
+        ]
+        for node_type in node_types:
+            try:
+                match_result = node_type.match(partial_proto_content)
+            except (ValueError, IndexError, TypeError):
+                raise ValueError(
+                    f"Could not parse proto content:\n{partial_proto_content}"
+                )
+            if match_result is not None:
+                return match_result
+        raise ValueError(f"Could not parse proto content:\n{partial_proto_content}")
+
+    @staticmethod
+    def parse_syntax_and_preceding_comments(
+        proto_content: str,
+    ) -> tuple[ProtoSyntax, Sequence[ProtoComment], str]:
+        # First, parse any preceding comments.
+        parsed_tree = []
+        while True:
+            for node_type in [ProtoSingleLineComment, ProtoMultiLineComment]:
+                try:
+                    match_result = node_type.match(proto_content)
+                except (ValueError, IndexError, TypeError):
+                    raise ValueError(f"Could not parse proto content:\n{proto_content}")
+                if match_result is not None:
+                    parsed_tree.append(match_result.node)
+                    proto_content = match_result.remaining_source.strip()
+                    break
+            if match_result is None:
+                break
+
+        # Next, parse syntax.
+        try:
+            syntax_match = ProtoSyntax.match(proto_content.strip())
+        except (ValueError, IndexError, TypeError):
+            raise ValueError(f"Proto doesn't have parseable syntax:\n{proto_content}")
+        if syntax_match is None:
+            raise ValueError(f"Proto doesn't have parseable syntax:\n{proto_content}")
+        syntax = syntax_match.node
+        proto_content = syntax_match.remaining_source.strip()
+
+        return syntax, parsed_tree, proto_content
+
+    @classmethod
+    def match(
+        cls, proto_content: str, parent: Optional["ProtoNode"] = None
+    ) -> Optional[ParsedProtoFileNode]:
+        syntax, parsed_tree, proto_content = cls.parse_syntax_and_preceding_comments(
+            proto_content
+        )
+        new_tree: list[ProtoNode] = list(parsed_tree)
+        while proto_content:
+            # Remove empty statements.
+            if proto_content.startswith(";"):
+                proto_content = proto_content[1:].strip()
+                continue
+            match_result = cls.parse_partial_content(proto_content)
+            new_tree.append(match_result.node)
+            proto_content = match_result.remaining_source.strip()
+
+        return ParsedProtoFileNode(cls(syntax, new_tree), proto_content)
+
+    def normalize(self) -> Optional["ProtoNode"]:
+        normalized_nodes = [n.normalize() for n in self.nodes]
+        return ProtoFile(
+            syntax=self.syntax.normalize(),
+            nodes=[n for n in normalized_nodes if n is not None],
+        )
 
     def serialize(self) -> str:
         serialized_parts = [self.syntax.serialize()]
--- src/util/parser.py
+++ src/util/parser.py
@@ -1,21 +1,6 @@
 import sys
-from typing import Sequence
 
-from src.proto_comment import (
-    ProtoComment,
-    ProtoMultiLineComment,
-    ProtoSingleLineComment,
-)
-from src.proto_enum import ProtoEnum
-from src.proto_extend import ProtoExtend
 from src.proto_file import ProtoFile
-from src.proto_import import ProtoImport
-from src.proto_message import ProtoMessage
-from src.proto_node import ParsedProtoNode, ProtoNode
-from src.proto_option import ProtoOption
-from src.proto_package import ProtoPackage
-from src.proto_service import ProtoService
-from src.proto_syntax import ProtoSyntax
 
 
 class ParseError(ValueError):
@@ -24,76 +9,17 @@ class ParseError(ValueError):
 
 class Parser:
     @staticmethod
-    def parse_partial_content(partial_proto_content: str) -> ParsedProtoNode:
-        node_types: list[type[ProtoNode]] = [
-            ProtoImport,
-            ProtoMessage,
-            ProtoPackage,
-            ProtoOption,
-            ProtoEnum,
-            ProtoExtend,
-            ProtoService,
-            ProtoSingleLineComment,
-            ProtoMultiLineComment,
-        ]
-        for node_type in node_types:
-            try:
-                match_result = node_type.match(partial_proto_content)
-            except (ValueError, IndexError, TypeError):
-                raise ParseError(
-                    f"Could not parse proto content:\n{partial_proto_content}"
-                )
-            if match_result is not None:
-                return match_result
-        raise ParseError(f"Could not parse proto content:\n{partial_proto_content}")
-
-    @staticmethod
-    def parse_syntax_and_preceding_comments(
-        proto_content: str,
-    ) -> tuple[ProtoSyntax, Sequence[ProtoComment], str]:
-        # First, parse any preceding comments.
-        parsed_tree = []
-        while True:
-            for node_type in [ProtoSingleLineComment, ProtoMultiLineComment]:
-                try:
-                    match_result = node_type.match(proto_content)
-                except (ValueError, IndexError, TypeError):
-                    raise ParseError(f"Could not parse proto content:\n{proto_content}")
-                if match_result is not None:
-                    parsed_tree.append(match_result.node)
-                    proto_content = match_result.remaining_source.strip()
-                    break
-            if match_result is None:
-                break
-
-        # Next, parse syntax.
-        try:
-            syntax_match = ProtoSyntax.match(proto_content.strip())
-        except (ValueError, IndexError, TypeError):
-            raise ParseError(f"Proto doesn't have parseable syntax:\n{proto_content}")
-        if syntax_match is None:
-            raise ParseError(f"Proto doesn't have parseable syntax:\n{proto_content}")
-        syntax = syntax_match.node
-        proto_content = syntax_match.remaining_source.strip()
-
-        return syntax, parsed_tree, proto_content
-
-    @staticmethod
     def loads(proto_content: str) -> ProtoFile:
-        syntax, parsed_tree, proto_content = Parser.parse_syntax_and_preceding_comments(
-            proto_content
-        )
-        new_tree: list[ProtoNode] = list(parsed_tree)
-        while proto_content:
-            # Remove empty statements.
-            if proto_content.startswith(";"):
-                proto_content = proto_content[1:].strip()
-                continue
-            match_result = Parser.parse_partial_content(proto_content)
-            new_tree.append(match_result.node)
-            proto_content = match_result.remaining_source.strip()
+        try:
+            parsed_file = ProtoFile.match(proto_content, None)
+        except ValueError as e:
+            raise ParseError(
+                f"Proto doesn't have parseable syntax:\n{proto_content}\n{e}"
+            )
+        if parsed_file is None:
+            raise ParseError(f"Proto doesn't have parseable syntax:\n{proto_content}")
 
-        return ProtoFile(syntax, new_tree)
+        return parsed_file.node
 
 
 if __name__ == "__main__":
