ce7b02299e0234605e86e74909a9b79c2d6f6434
Charles OuGuo <shaldengeki@gmail.com>
2023-03-09 10:56:15 -0400

    Make Extend a ProtoContainerNode

--- src/proto_extend.py
+++ src/proto_extend.py
@@ -5,28 +5,27 @@ from src.proto_comment import (
     ProtoMultiLineComment,
     ProtoSingleLineComment,
 )
-from src.proto_identifier import ProtoEnumOrMessageIdentifier
+from src.proto_identifier import (
+    ParsedProtoEnumOrMessageIdentifierNode,
+    ProtoEnumOrMessageIdentifier,
+)
 from src.proto_message_field import ProtoMessageField
-from src.proto_node import ParsedProtoNode, ProtoNode
+from src.proto_node import ParsedProtoNode, ProtoContainerNode, ProtoNode
 
 
-class ProtoExtend(ProtoNode):
+class ProtoExtend(ProtoContainerNode):
     def __init__(
         self,
         name: ProtoEnumOrMessageIdentifier,
-        nodes: list[ProtoNode],
         *args,
         **kwargs,
     ):
         super().__init__(*args, **kwargs)
         self.name = name
         self.name.parent = self
-        self.nodes = nodes
-        for node in self.nodes:
-            node.parent = self
 
     def __eq__(self, other) -> bool:
-        return self.name == other.name and self.nodes == other.nodes
+        return super().__eq__(other) and self.name == other.name
 
     def __str__(self) -> str:
         return f"<ProtoExtend name={self.name}, nodes={self.nodes}>"
@@ -44,28 +43,20 @@ class ProtoExtend(ProtoNode):
             parent=self.parent,
         )
 
-    @staticmethod
-    def parse_partial_content(partial_content: str) -> ParsedProtoNode:
-        supported_types: list[type[ProtoNode]] = [
+    @classmethod
+    def container_types(cls) -> list[type[ProtoNode]]:
+        return [
             ProtoSingleLineComment,
             ProtoMultiLineComment,
             ProtoMessageField,
         ]
-        for node_type in supported_types:
-            try:
-                match_result = node_type.match(partial_content)
-            except (ValueError, IndexError, TypeError):
-                raise ValueError(
-                    f"Could not parse partial extend content:\n{partial_content}"
-                )
-            if match_result is not None:
-                return match_result
-        raise ValueError(f"Could not parse partial extend content:\n{partial_content}")
 
     @classmethod
-    def match(
-        cls, proto_source: str, parent: Optional[ProtoNode] = None
-    ) -> Optional["ParsedProtoNode"]:
+    def match_header(
+        cls,
+        proto_source: str,
+        parent: Optional["ProtoNode"] = None,
+    ) -> Optional["ParsedProtoEnumOrMessageIdentifierNode"]:
         if not proto_source.startswith("extend "):
             return None
 
@@ -82,25 +73,29 @@ class ProtoExtend(ProtoNode):
                 f"Proto extend has invalid syntax, expecting opening curly brace: {proto_source}"
             )
 
-        proto_source = proto_source[1:].strip()
-        parsed_tree = []
-        while proto_source:
-            # Remove empty statements.
-            if proto_source.startswith(";"):
-                proto_source = proto_source[1:].strip()
-                continue
+        return ParsedProtoEnumOrMessageIdentifierNode(name, proto_source[1:].strip())
 
-            if proto_source.startswith("}"):
-                proto_source = proto_source[1:].strip()
-                break
+    @classmethod
+    def match_footer(
+        cls,
+        proto_source: str,
+        parent: Optional[ProtoNode] = None,
+    ) -> Optional[str]:
+        if proto_source.startswith("}"):
+            return proto_source[1:].strip()
 
-            match_result = ProtoExtend.parse_partial_content(proto_source)
-            parsed_tree.append(match_result.node)
-            proto_source = match_result.remaining_source.strip()
+        return None
 
-        return ParsedProtoNode(
-            ProtoExtend(name=name, nodes=parsed_tree, parent=parent), proto_source
-        )
+    @classmethod
+    def construct(
+        cls,
+        header_match: ParsedProtoNode,
+        contained_nodes: list[ProtoNode],
+        footer_match: str,
+        parent: Optional[ProtoNode] = None,
+    ) -> ProtoNode:
+        assert isinstance(header_match, ParsedProtoEnumOrMessageIdentifierNode)
+        return ProtoExtend(name=header_match.node, nodes=contained_nodes, parent=parent)
 
     def serialize(self) -> str:
         serialize_parts = (
