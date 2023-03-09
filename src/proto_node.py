64029f5ea27ae31351ffea5de308fcb6ea2d2a24
Charles OuGuo <shaldengeki@gmail.com>
2023-03-09 10:33:13 -0400

    Add ProtoContainerNode

--- src/proto_node.py
+++ src/proto_node.py
@@ -1,5 +1,5 @@
 import abc
-from typing import NamedTuple, Optional
+from typing import NamedTuple, Optional, Type
 
 
 class ProtoNode(abc.ABC):
@@ -22,6 +22,86 @@ class ProtoNode(abc.ABC):
     @abc.abstractmethod
     def normalize(self) -> Optional["ProtoNode"]:
         raise NotImplementedError
+
+
+class ProtoContainerNode(ProtoNode):
+    @classmethod
+    @abc.abstractmethod
+    def match_header(
+        cls,
+        proto_source: str,
+        parent: Optional["ProtoNode"] = None,
+    ) -> Optional["ParsedProtoNode"]:
+        raise NotImplementedError
+
+    @classmethod
+    @abc.abstractmethod
+    def match_footer(
+        cls,
+        proto_source: str,
+        parent: Optional["ProtoNode"] = None,
+    ) -> Optional["ParsedProtoNode"]:
+        raise NotImplementedError
+
+    @classmethod
+    @abc.abstractmethod
+    def container_types(cls) -> list[type[ProtoNode]]:
+        raise NotImplementedError
+
+    @classmethod
+    @abc.abstractmethod
+    def construct(
+        cls,
+        header_match: "ParsedProtoNode",
+        contained_nodes: list[ProtoNode],
+        footer_match: "ParsedProtoNode",
+    ):
+        raise NotImplementedError
+
+    @classmethod
+    def parse_partial_content(cls, partial_content: str) -> "ParsedProtoNode":
+        for node_type in cls.container_types():
+            try:
+                match_result = node_type.match(partial_content)
+            except (ValueError, IndexError, TypeError):
+                raise ValueError(f"Could not parse partial content:\n{partial_content}")
+            if match_result is not None:
+                return match_result
+        raise ValueError(f"Could not parse partial content:\n{partial_content}")
+
+    @classmethod
+    def match(
+        cls,
+        proto_source: str,
+        parent: Optional["ProtoNode"] = None,
+    ) -> Optional["ParsedProtoNode"]:
+        header_match = cls.match_header(proto_source, parent=parent)
+        if header_match is None:
+            return None
+
+        proto_source = header_match.remaining_source.strip()
+        nodes = []
+        while proto_source:
+            # Remove empty statements.
+            if proto_source.startswith(";"):
+                proto_source = proto_source[1:].strip()
+                continue
+
+            footer_match = cls.match_footer(proto_source, parent)
+            if footer_match is not None:
+                proto_source = footer_match.remaining_source.strip()
+                break
+
+            match_result = cls.parse_partial_content(proto_source)
+            nodes.append(match_result.node)
+            proto_source = match_result.remaining_source.strip()
+
+        if footer_match is None:
+            raise ValueError(
+                f"Footer was not found when matching container node {cls} for remaining proto source {proto_source}"
+            )
+
+        return cls.construct(header_match, nodes, footer_match)
 
 
 class ParsedProtoNode(NamedTuple):
